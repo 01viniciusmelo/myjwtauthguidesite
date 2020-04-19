bb4c0b8ec070bc1131869c844b66b5840c6d52f9
Vinícius Lopes de Melo <viniciusldemelo@gmail.com>
2020-04-19 16:11:45 -0300

    SQL user admin

---
+++ sql_user_admin.sql
@@ -0,0 +1,4 @@
+INSERT INTO myjwtauthguidesite.users (name,email,email_verified_at,password,remember_token,created_at,updated_at) VALUES 
+('Admin','admin@admin.com',NULL,'$2y$10$NzC7cV7Bj3XwhO8NvjgdM.VxD8NdeR56o/RlBG4YyRMCvxYyCs/9O',NULL,'2020-04-19 16:16:09.000','2020-04-19 16:16:09.000');
+
+--Senha: password
